97e0f1d02029c7ccafa629aa3d1f0f77fa872c22
gonzalopozo <gonzalopozo2005@gmail.com>
2025-04-07 09:34:47 +0200

    Uso de st.file_uploader()

--- carga_archivo.py
+++ carga_archivo.py
@@ -9,7 +9,7 @@ def load_image(image_file):
     img = Image.open(image_file)
     return img
 
-def leer_pdf(pdf_file):
+def read_pdf(pdf_file):
     pdfReader = PdfReader(pdf_file)
     count = len(pdfReader.pages)
     all_the_text = ""
@@ -19,3 +19,27 @@ def leer_pdf(pdf_file):
         all_the_text += page.extract_text()
     
     return all_the_text
+
+def main():
+    st.title("Carga de archivos:")
+
+    menu = ["Imágenes", "Conjunto de datos", "Archivos de documentos"]
+
+    selectSidebar = st.sidebar.selectbox("Menú", menu)
+
+    match selectSidebar:
+        case "Imágenes":
+            st.subheader("Imágenes")
+            img_file = st.file_uploader("Subir imágen", type=['png', 'jpg', 'jpeg'])
+
+        case "Conjunto de datos":
+            st.subheader("Conjunto de datos")
+
+        case "Archivos de documentos":
+            st.subheader("Archivos de documentos")
+
+        case _:
+            st.subheader("ERROR")
+
+if __name__ == '__main__':
+    main()
