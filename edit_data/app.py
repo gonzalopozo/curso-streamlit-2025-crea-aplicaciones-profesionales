87e11ae532f96e89161152f2def5746aee938f28
gonzalopozo <gonzalopozo2005@gmail.com>
2025-04-08 16:53:23 +0200

    Uso de st.data_editor() de formas más profesional usando un st.form() para guardar los datos, y una vez guardadose habilitará un botón de descarga, para poder descargar el archivo modificado

--- edit_data/app.py
+++ edit_data/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import time
 
 def main():
     st.header("Editor de datos")
@@ -8,14 +9,23 @@ def main():
     csv_file = st.file_uploader('Subir archivo CSV', ['csv'])
 
     if csv_file:
-        modified_csv_file = st.data_editor(pd.read_csv(csv_file))
+        with st.form("formulario_edicion"):
+            df = pd.read_csv (csv_file)
+            modified_df = st.data_editor(df)
+            save_modified_df = st.form_submit_button("¡Guardar datos!")
 
-        st.download_button(
-            label='Descargatelo manin',
-            data=modified_csv_file.to_csv(),
-            file_name='datos_nba.csv',
-            mime='text/csv'
-        )
+        if save_modified_df:
+            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            new_file_name = f"{csv_file.name.split('.')[0]}_{timestamp}.csv"
+
+            final_df = modified_df.to_csv(index=False, encoding='utf-8')
+            
+            st.download_button(
+                label='Descargar datos como CSV',
+                data=final_df.encode('utf-8'),
+                file_name=new_file_name,
+                mime='text/csv'
+            )
 
 if __name__ == "__main__":
     main()
